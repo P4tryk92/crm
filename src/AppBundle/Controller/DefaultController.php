47c93d367228df096187a09be3f467d67fa62f9f
P4tryk92 <66371791@wp.pl>
2019-07-01 21:34:11 +0200

    logowanie i tworzenie konta

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,35 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
-    }
-
-    /**
-     * @Route("/register", name="register")
-     */
-    public function registerAction(UserPasswordEncoderInterface $encoder)
-    {
-        // whatever *your* User object is
-        $user = new AppBundle\Entity\User();
-        $plainPassword = 'admin';
-        $encoded = $encoder->encodePassword($user, $plainPassword);
-
-        $user->setPassword($encoded);
-
-        dump($user);
-        exit;
+        return $this->render('default/index.html.twig');
     }
 }
---
+++ src/AppBundle/Form/UserNewType.php
@@ -0,0 +1,41 @@
+<?php
+ 
+namespace AppBundle\Form;
+ 
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Length;
+ 
+class UserNewType extends AbstractType
+{   
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class, ['label' => 'Imię', 'attr' => ['class' => 'form-style']])
+            ->add('lastname', TextType::class, ['label' => 'Nazwisko', 'attr' => ['class' => 'form-style']])
+            ->add('pin', NumberType::class, ['label' => 'Pesel', 'attr' => ['class' => 'form-style'], 'constraints' => [new Length(['min' => 11, 'max' => 11])]])
+            ->add('save', SubmitType::class, ['label' => 'Zapisz', 'attr' => ['class' => 'form-style']])
+            ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'UserNew';
+    }
+}
