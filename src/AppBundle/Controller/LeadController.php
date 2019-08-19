a718fcb9b5173f2b1d0debf288007c644cbc403f
P4tryk92 <66371791@wp.pl>
2019-08-19 21:14:54 +0200

    majatek wyceniony przez rzeczoznawce

--- src/AppBundle/Controller/LeadController.php
+++ src/AppBundle/Controller/LeadController.php
@@ -8,8 +8,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\Lead;
 use AppBundle\Entity\Credit;
+use AppBundle\Entity\WealthByAppraiser;
 use AppBundle\Form\LeadType;
 use AppBundle\Form\CreditType;
+use AppBundle\Form\WealthByAppraiserType;
 use AppBundle\Controller\BaseController;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 
@@ -199,6 +201,110 @@ class LeadController extends BaseController
         return $this->render('lead/credit/index.html.twig', [
             'lead'  => $lead,
             'credits' => $paginator,
+            'perPage' => $this->perPage,
+            'page' => $page,
+        ]);
+    }
+
+    public function newWealthByAppraiserAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $lead = $em->getRepository('AppBundle:Lead')->findOneById($id);
+
+        if (!$lead) {
+            return $this->redirectToRoute('lead_index'); 
+        }
+
+        $wealthByAppraiser = new WealthByAppraiser();
+
+        $form = $this->createForm(WealthByAppraiserType::class, $wealthByAppraiser);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $wealthByAppraiser = $form->getData();
+            $wealthByAppraiser->setLead($lead);
+            
+            $em->persist($wealthByAppraiser);
+            $em->flush();
+
+            return $this->redirectToRoute('lead_wealth_by_appraiser_index', ['id' => $lead->getId()]);
+        }
+
+        return $this->render('lead/wealthByAppraiser/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function editWealthByAppraiserAction(Request $request, $leadId, $wealthByAppraiserId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $lead = $em->getRepository('AppBundle:Lead')->findOneById($leadId);
+        $wealthByAppraiser = $em->getRepository('AppBundle:wealthByAppraiser')->findOneById($wealthByAppraiserId);
+
+        if (!$lead || !$wealthByAppraiser) {
+            return $this->redirectToRoute('lead_index'); 
+        }
+
+        $form = $this->createForm(WealthByAppraiserType::class, $wealthByAppraiser);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $wealthByAppraiser = $form->getData();
+            $wealthByAppraiser->setLead($lead);
+            
+            $em->persist($wealthByAppraiser);
+            $em->flush();
+
+            return $this->redirectToRoute('lead_wealth_by_appraiser_index', ['id' => $lead->getId()]);
+        }
+
+        return $this->render('lead/wealthByAppraiser/new.html.twig', [
+            'form' => $form->createView(),
+            'edit' => 1
+        ]);
+    }
+
+    public function removeWealthByAppraiserAction(Request $request, $leadId, $wealthByAppraiserId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $lead = $em->getRepository('AppBundle:Lead')->findOneById($leadId);
+        $wealthByAppraiser = $em->getRepository('AppBundle:wealthByAppraiser')->findOneById($wealthByAppraiserId);
+
+        if (!$lead || !$wealthByAppraiser) {
+            return $this->redirectToRoute('lead_index'); 
+        }
+
+        $em->remove($wealthByAppraiser);
+        $em->flush();
+
+        return $this->redirectToRoute('lead_wealth_by_appraiser_index', ['id' => $lead->getId()]);
+    }
+
+    public function indexWealthByAppraiserAction(Request $request, $id, $page)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $lead = $em->getRepository('AppBundle:Lead')->findOneById($id);
+
+        if (!$lead) {
+            return $this->redirectToRoute('lead_index'); 
+        }
+
+        $this->setPerPage();
+
+        $query = $this->getDoctrine()->getRepository('AppBundle:wealthByAppraiser')
+                    ->createQueryBuilder('c')
+                    ->where('c.lead = :leadId')
+                    ->setParameter('leadId', $id)
+                    ->getQuery()
+                    ->setFirstResult(($page-1) * $this->perPage)
+                    ->setMaxResults($this->perPage);
+
+        $paginator = new Paginator($query);
+        
+        return $this->render('lead/wealthByAppraiser/index.html.twig', [
+            'lead'  => $lead,
+            'wealthsByAppraiser' => $paginator,
             'perPage' => $this->perPage,
             'page' => $page,
         ]);
