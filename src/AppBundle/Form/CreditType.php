9976d59d0612195c087bdfa898fb962646d726a9
Konrad Piech <k9602@o2.pl>
2019-08-29 18:25:33 +0200

    edycja szablonu kredytu, zmiana typów inputów

--- src/AppBundle/Form/CreditType.php
+++ src/AppBundle/Form/CreditType.php
@@ -8,19 +8,20 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
  
 class CreditType extends AbstractType
 {   
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('amount', NumberType::class, ['label' => 'Kwota', 'attr' => ['class' => 'form-control']])
+            ->add('amount', IntegerType::class, ['label' => 'Kwota', 'attr' => ['class' => 'form-control','step' => '0.01']])
             ->add('grantingDate', DateType::class, ['widget' => 'single_text', 'label' => 'Data udzielenia kredytu', 'attr' => ['class' => 'form-control']])
             ->add('repaymentDate', DateType::class, ['widget' => 'single_text', 'label' => 'Data spłaty kredytu', 'attr' => ['class' => 'form-control']])
-            ->add('monthlyInstallment', TextType::class, ['label' => 'Miesięczna rata', 'attr' => ['class' => 'form-control']])
+            ->add('monthlyInstallment', IntegerType::class, ['label' => 'Miesięczna rata', 'attr' => ['class' => 'form-control','step' => '0.01']])
             ->add('bankName', TextType::class, ['label' => 'Nazwa banku', 'attr' => ['class' => 'form-control']])
-            ->add('profitMargin', NumberType::class, ['label' => 'Wysokość marży kredytu', 'attr' => ['class' => 'form-control']])
-            ->add('save', SubmitType::class, ['label' => 'Zapisz', 'attr' => ['class' => 'form-control']])
+            ->add('profitMargin', NumberType::class, ['label' => 'Wysokość marży kredytu', 'attr' => ['class' => 'form-control','step' => '0.01']])
+            ->add('save', SubmitType::class, ['label' => 'Zapisz', 'attr' => ['class' => 'btn btn-danger btn-sm']])
             ;
     }
     
