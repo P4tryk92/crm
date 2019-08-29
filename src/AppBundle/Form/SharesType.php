2e405152cd2640272de7890532cf01a382977916
Konrad Piech <k9602@o2.pl>
2019-08-29 18:40:07 +0200

    edycja szablonu akcji, zmiana typów inputów

--- src/AppBundle/Form/SharesType.php
+++ src/AppBundle/Form/SharesType.php
@@ -5,7 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
  
 class SharesType extends AbstractType
@@ -14,9 +14,9 @@ class SharesType extends AbstractType
     {
         $builder
             ->add('nameAndType', TextType::class, ['label' => 'Nazwa i rodzaj', 'attr' => ['class' => 'form-control']])
-            ->add('numberOfSharesHeld', NumberType::class, ['label' => 'Ilość', 'attr' => ['class' => 'form-control']])
-            ->add('value', NumberType::class, ['label' => 'Wartość', 'attr' => ['class' => 'form-control']])
-            ->add('save', SubmitType::class, ['label' => 'Zapisz', 'attr' => ['class' => 'form-control']])
+            ->add('numberOfSharesHeld', IntegerType::class, ['label' => 'Ilość', 'attr' => ['class' => 'form-control']])
+            ->add('value', IntegerType::class, ['label' => 'Wartość', 'attr' => ['class' => 'form-control','step' => '0.01']])
+            ->add('save', SubmitType::class, ['label' => 'Zapisz', 'attr' => ['class' => 'btn btn-danger btn-sm']])
             ;
     }
     
